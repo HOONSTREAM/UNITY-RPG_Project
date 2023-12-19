c28d7eeb0a52bb27a3818d199d5dc11093a40bfc
SEUNG_HOON_LEE <123620703+HOONSTREAM@users.noreply.github.com>
2023-12-20 03:16:29 +0900

    23.12.20 UPDATED

--- Assets/Scripts/NPCManager/Rudencia_in_Script.cs
+++ Assets/Scripts/NPCManager/Rudencia_in_Script.cs
@@ -6,11 +6,25 @@ using UnityEngine.SceneManagement;
 
 public class Rudencia_in_Script : MonoBehaviour
 {
+
     private void OnTriggerEnter(Collider other)
     {
+        //GameObject.Find 함수는 비활성화 된 오브젝트는 찾지 못한다.
+        GameObject[] allObjects = Resources.FindObjectsOfTypeAll<GameObject>(); //비활성화 및 Don'tDestoryOnLoad 까지 찾는다.
+
+        foreach (var obj in allObjects)
+        {
+            if (obj.name == "NPC Folder_Rudencia")
+            {
+                obj.SetActive(true);
+                break;
+            }
+         
+        }
+
         GameObject player = Managers.Game.GetPlayer();
         player.transform.position = new Vector3(8.4f, 0, -43.34f);
-       
+      
         SceneManager.LoadScene("루덴시안");
     }
 }
--- Assets/Scripts/NPCManager/shop_in_script.cs
+++ Assets/Scripts/NPCManager/shop_in_script.cs
@@ -14,7 +14,8 @@ public class shop_in_script : MonoBehaviour
         GameObject player = Managers.Game.GetPlayer();
         DontDestroyOnLoad(player);
         DontDestroyOnLoad(savedata);
-       
+
+        GameObject.Find("NPC Folder_Rudencia").gameObject.SetActive(false); // 루덴시아 관련 NPC Off
         SceneManager.LoadScene("루덴시안 상점");
     }
 
