5732e619dc29c4aa382b30e1e61724a5a9b7b4b9
SEUNG_HOON_LEE <tmdgns7469@gmail.com>
2024-02-07 04:17:42 +0900

    24.02.07 UPDATED
    -

--- Assets/Scripts/UI/Exit_Console.cs
+++ Assets/Scripts/UI/Exit_Console.cs
@@ -8,10 +8,35 @@ public class Exit_Console : MonoBehaviour
 {
     public GameObject ExitConsole;
 
+    public GameObject INVENTORY_CANVAS;
+    public GameObject QuestSlot_CANVAS;
+    public GameObject Abillity_CANVAS;
+    public GameObject Storage_CANVAS;
+
+    public GameObject InvenUI;
+    public GameObject EquipmentUI;
+    public GameObject QuestSlotUI;
+    public GameObject Abillity_slot_UI;
+    public GameObject Storage_slot_UI;
+
     private void Update()
     {
         if (Input.GetKeyDown(KeyCode.Escape))
         {
+            if(InvenUI.activeSelf || EquipmentUI.activeSelf || QuestSlotUI.activeSelf || Abillity_slot_UI.activeSelf || Storage_slot_UI.activeSelf)
+            {
+                Debug.Log("먼저 열려있는 창이 있다.");
+
+                int inven_sort_order = INVENTORY_CANVAS.gameObject.GetComponent<Canvas>().sortingOrder;
+                int quest_sort_order = QuestSlot_CANVAS.gameObject.GetComponent<Canvas>().sortingOrder;
+                int abillity_sort_order = Abillity_CANVAS.gameObject.GetComponent<Canvas>().sortingOrder;
+                int storage_sort_order = Storage_CANVAS.gameObject.GetComponent<Canvas>().sortingOrder;
+
+                int max_sort_order = Mathf.Max(inven_sort_order, quest_sort_order, abillity_sort_order, storage_sort_order);
+
+                Debug.Log(max_sort_order);
+            }
+
             Managers.Sound.Play("Coin");
             if (ExitConsole.gameObject.activeSelf)
             {
