065fdf7d91f120f984238a2b08325a715620874f
SEUNG_HOON_LEE <123620703+HOONSTREAM@users.noreply.github.com>
2024-03-16 20:39:45 +0900

    24.03.16 UPDATED

--- Assets/Scripts/Scenes/LoginScene.cs
+++ Assets/Scripts/Scenes/LoginScene.cs
@@ -2,28 +2,84 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
-public class LoginScene : BaseScene
+public class LoginScene : MonoBehaviour
 {
+    public InputField ID;
+    public InputField Password;
 
-    protected override void Init()
+    public Text notify;
+
+    private void Start()
     {
-        base.Init();
-        SceneType = Define.Scene.Login;
-       
+        notify.text = "";
     }
 
-    private void Update()
+    private bool CheckInput(string id, string password)
     {
-        if (Input.GetKeyDown(KeyCode.Q))
+        if (id == "" || password == "")
         {
-            Managers.Scene.LoadScene(Define.Scene.Rudencia);
+            notify.text = "아이디 또는 패스워드를 입력하세요.";
+            ID.text = "";
+            Password.text = "";
+            return false;
+        }
+
+        else
+        {
+            return true;
+        }
+
+    }
+    public void SaveUserData()
+    {
+        if (!CheckInput(ID.text, Password.text))
+        {
+            return;
+        }
+        // 사용자의 아이디는 Key로 패스워드를 값(value)로 설정해서 저장함.
+
+
+        if (!PlayerPrefs.HasKey(ID.text))
+        {
+            PlayerPrefs.SetString(ID.text, Password.text);
+            notify.text = "계정생성이 완료되었습니다.";
+            ID.text = "";
+            Password.text = "";
+
+        }
+
+        else
+        {
+            notify.text = "이미 존재하는 계정입니다.";
+            ID.text = "";
+            Password.text = "";
+        }
+
+    }
+
+    public void CheckUserData()
+    {
+
+        if (!CheckInput(ID.text, Password.text))
+        {
+            return;
+        }
+
+        string pass = PlayerPrefs.GetString(ID.text);
+
+        if (Password.text == pass)
+        {
+            SceneManager.LoadScene(1);
+        }
+
+        else
+        {
+            notify.text = "입력하신 아이디와 패스워드가 불일치 합니다.";
+            ID.text = "";
+            Password.text = "";
         }
     }
-    public override void Clear()
-    {
-        Debug.Log("LoginScene Clear!");
-    }
 
-   
 }
