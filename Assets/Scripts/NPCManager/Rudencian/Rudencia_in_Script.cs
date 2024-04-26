2d7dcff8c267ccb14ab1ed4c754b30285ccdc4ec
SEUNG_HOON_LEE <123620703+HOONSTREAM@users.noreply.github.com>
2024-04-26 16:13:15 +0900

    24.04.26 UPDATED

--- Assets/Scripts/NPCManager/Rudencian/Rudencia_in_Script.cs
+++ Assets/Scripts/NPCManager/Rudencian/Rudencia_in_Script.cs
@@ -22,7 +22,7 @@ public class Rudencia_in_Script : MonoBehaviour
 
             case "루덴시안 보석상":
 
-                player.transform.position = new Vector3(-1.043379f, 0, -2.187184f);
+                player.transform.position = new Vector3(-15.118f, 0, -10.432f);
                 pc.State = Define.State.Idle;      
                 
                 SceneManager.LoadScene(LoadingScene.LOADING_SCENE_NUMBER);
@@ -30,7 +30,7 @@ public class Rudencia_in_Script : MonoBehaviour
 
             case "루덴시안 남쪽":
                
-                player.transform.position = new Vector3(0.9320679f, 0, -47.52995f);               
+                player.transform.position = new Vector3(23.25f, 0, -41.45f);               
                 pc.State = Define.State.Idle;
             
                 SceneManager.LoadScene(LoadingScene.LOADING_SCENE_NUMBER);
@@ -38,7 +38,7 @@ public class Rudencia_in_Script : MonoBehaviour
 
             case "루덴시안 상점":
             
-                player.transform.position = new Vector3(8.4f, 0, -43.34f);              
+                player.transform.position = new Vector3(23.25f, 0, -33.15f);              
                 pc.State = Define.State.Idle;
 
                 SceneManager.LoadScene(LoadingScene.LOADING_SCENE_NUMBER);
@@ -46,7 +46,7 @@ public class Rudencia_in_Script : MonoBehaviour
 
             case "루덴시안 은행":
                
-                player.transform.position = new Vector3(-8.585f, 0, -20.935f);            
+                player.transform.position = new Vector3(-4.93f, 0, -30.87f);            
                 pc.State = Define.State.Idle;
 
                 SceneManager.LoadScene(LoadingScene.LOADING_SCENE_NUMBER);
@@ -55,8 +55,8 @@ public class Rudencia_in_Script : MonoBehaviour
             case "루덴시안 여관":
 
              
-                player.transform.position = new Vector3(7.54f, 0, -10.74f);
-                player.transform.rotation = new Quaternion(0, 274.886f, 0, 0);
+                player.transform.position = new Vector3(-1.06f, 0, -10.24f);
+                player.transform.rotation = new Quaternion(0, 180f, 0, 0);
                 pc.State = Define.State.Idle;
 
                 SceneManager.LoadScene(LoadingScene.LOADING_SCENE_NUMBER);
