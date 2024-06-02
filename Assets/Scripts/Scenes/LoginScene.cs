b862c736031a3c6757ae900838fe8509e4fcc8f1
SEUNG_HOON_LEE <tmdgns7469@gmail.com>
2024-06-02 19:13:30 +0900

    24.06.02 UPDATED
    -

--- Assets/Scripts/Scenes/LoginScene.cs
+++ Assets/Scripts/Scenes/LoginScene.cs
@@ -10,9 +10,7 @@ public class LoginScene : MonoBehaviour
 
     private void Start()
     {
-        LoadingScene.NEXT_SCENE_NUMBER = Managers.Scene_Number.Get_Start_Scene();
-        GameObject Init_player = Managers.Game.GetPlayer();
-        Camera.main.gameObject.GetAddComponent<CameraController>().SetPlayer(Init_player);
+        LoadingScene.NEXT_SCENE_NUMBER = Managers.Scene_Number.Get_Start_Scene();     
         gameObject.GetAddComponent<CursorController>();
        
     }
@@ -26,12 +24,28 @@ public class LoginScene : MonoBehaviour
 
 
     public void Load_Game()
-    {
-        
+    {       
         SceneManager.LoadScene(1);
+
+        StartCoroutine(wait_Load_PlayerData());
     }
 
    
+    IEnumerator wait_Load_PlayerData()
+    {
+        yield return new WaitForSeconds(0.5f);
+
+        if (Managers.Game.GetPlayer() == null)
+        {
+            Debug.Log("플레이어가 없음");
+        }
+        else
+        {
+            Debug.Log("플레이어 로드 데이터 메서드 진입 전");
+            Managers.Save.LoadPlayerData();
+            Debug.Log("플레이어 로드 데이터 메서드 호출 완료");
+        }
+    }
     public void Game_Exit()
     {
 #if UNITY_EDITOR
